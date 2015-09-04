ee3b2a38078e91232d146c392e113b01cd44e5ce
Jozef Izso <jozef.izso@gmail.com>
2015-09-04 23:27:53 +0200

    Added support for Azure SDK 2.6 and 2.7

--- teamcity-microsoftsdks-agent/src/main/java/net/izsak/teamcity/WindowsAzureSdkDetector.java
+++ teamcity-microsoftsdks-agent/src/main/java/net/izsak/teamcity/WindowsAzureSdkDetector.java
@@ -11,7 +11,10 @@ public class WindowsAzureSdkDetector extends SdkDetector {
     private static final Logger LOG = Logger.getLogger(WindowsAzureSdkDetector.class);
 
     final String basePath = "SOFTWARE\\Microsoft\\Microsoft SDKs\\ServiceHosting\\";
-    final String[] versionsToDetect = new String[] { "1.0", "1.1", "1.2", "1.3", "1.4", "1.5", "1.6", "1.7", "1.8", "2.0", "2.1", "2.2", "2.3", "2.4", "2.5" };
+    final String[] versionsToDetect = new String[] {
+            "1.0", "1.1", "1.2", "1.3", "1.4", "1.5", "1.6", "1.7", "1.8",
+            "2.0", "2.1", "2.2", "2.3", "2.4", "2.5", "2.6", "2.7"
+    };
 
     private Win32RegistryAccessor registry;
 
